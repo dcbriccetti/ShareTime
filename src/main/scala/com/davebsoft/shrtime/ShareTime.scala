90a32bed7467f2e86c36ecffe44b53feee95d3d9
Dave Briccetti <daveb@davebsoft.com>
2018-06-18 08:57:33 -0700

    Get checkbox working to toggle student

--- src/main/scala/com/davebsoft/shrtime/ShareTime.scala
+++ src/main/scala/com/davebsoft/shrtime/ShareTime.scala
@@ -1,18 +1,35 @@
 package com.davebsoft.shrtime
 
 import org.querki.jquery._
+import org.scalajs.dom
 
 object ShareTime {
+  val student = Student()
+
   def main(args: Array[String]): Unit = {
-    $(() => setupUI())
+    $(() =>
+    setUpUi())
   }
 
-  def setupUI(): Unit = {
-    $("body").append("<p>Hello World</p>")
-    $("#click-me-button").click(() => addClickedMessage())
+  private def setUpUi() = {
+    $("#stu1-toggle").click(() => toggleStudent())
+    dom.window.setInterval(() => update(), 1000)
   }
 
-  def addClickedMessage(): Unit = {
-    $("body").append("<input type='checkbox'/>Student 1")
+  private def toggleStudent(): Unit = {
+    dom.console.log($("toggleStudent"))
+    student.active = !student.active
   }
+
+  def update(): Unit = {
+    if (student.active) {
+      student.secondsUsed += 1
+      $("body").append(s"<p>${student.secondsUsed}</p>")
+    }
+  }
+}
+
+case class Student() {
+  var secondsUsed = 0D
+  var active = false
 }
