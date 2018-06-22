a89911ec554c0e7f222824926ed3090914b9a195
Dave Briccetti <daveb@davebsoft.com>
2018-06-21 23:40:52 -0700

    Support multiple students

--- src/main/scala/com/davebsoft/shrtime/ShareTime.scala
+++ src/main/scala/com/davebsoft/shrtime/ShareTime.scala
@@ -3,33 +3,47 @@ package com.davebsoft.shrtime
 import org.querki.jquery._
 import org.scalajs.dom
 
-object ShareTime {
-  val student = Student()
+import scala.math.BigDecimal.RoundingMode.HALF_UP
 
-  def main(args: Array[String]): Unit = {
-    $(() =>
-    setUpUi())
-  }
+object ShareTime {
+  private var students = Seq[Student]()
+
+  def main(args: Array[String]): Unit = $(() => setUpUi())
 
   private def setUpUi() = {
-    $("#stu1-toggle").click(() => toggleStudent())
+    $("#add").click(() => addStudent())
     dom.window.setInterval(() => update(), 1000)
   }
 
-  private def toggleStudent(): Unit = {
-    dom.console.log($("toggleStudent"))
+  private def addStudent(): Unit = {
+    val newElement = $(".stu-div").clone().removeClass("stu-div").removeAttr("hidden")
+    val stuName = $("#stu-name").valueString
+    newElement.children(".checkbox-label").text(stuName)
+    val newStu = Student(stuName, newElement)
+    students :+= newStu
+    newElement.children(".stu-toggle").click(() => toggleStudent(newStu))
+    newElement.appendTo("#students")
+  }
+
+  private def toggleStudent(student: Student): Unit = {
     student.active = !student.active
   }
 
-  def update(): Unit = {
-    if (student.active) {
-      student.secondsUsed += 1
-      $("body").append(s"<p>${student.secondsUsed}</p>")
-    }
+  private def update(): Unit = {
+    val numActive = students.count(_.active)
+    if (numActive > 0)
+      students.foreach { student =>
+        if (student.active) {
+          student.minutesUsed += 1D / 60 / numActive
+          val mu = BigDecimal(student.minutesUsed).setScale(1, HALF_UP).toString
+          student.element.children(".stu-progress-bar").value(mu)
+          student.element.children(".stu-progress-num").text(mu)
+        }
+      }
   }
 }
 
-case class Student() {
-  var secondsUsed = 0D
+case class Student(name: String, element: JQuery) {
+  var minutesUsed = 0D
   var active = false
 }
