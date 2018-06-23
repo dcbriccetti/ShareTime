3799fbf526f74cccac53c1421c4137d3c03082db
Dave Briccetti <daveb@davebsoft.com>
2018-06-22 21:31:41 -0700

    Make some readability improvements

--- src/main/scala/com/davebsoft/shrtime/ShareTime.scala
+++ src/main/scala/com/davebsoft/shrtime/ShareTime.scala
@@ -12,31 +12,29 @@ object ShareTime {
   def main(args: Array[String]): Unit = $(() => setUpUi())
 
   private def setUpUi() = {
-    $("#add").click(() => {
+    $("#add-students").click(() => {
       val sn = $("#stu-name")
-      addStudents(sn.valueString)
+      sn.valueString.split(',').map(_.trim).foreach(addStudent)
       sn.value("")
     })
+
     val periodMinsElem = $("#period-mins")
     periodMinsElem.value(periodMins.toString)
     periodMinsElem.change(() => {
       periodMins = periodMinsElem.valueString.toInt
       students.foreach(_.progressBar.attr("max", periodMins))
     })
+
     dom.window.setInterval(() => update(), 1000)
   }
 
-  private def addStudents(stuNames: String): Unit = {
-    stuNames.split(',').foreach(s => addStudent(s.trim))
-  }
-
   private def addStudent(stuName: String): Unit = {
-    val newElement = $("#stu-tr-template").clone().removeAttr("id").removeAttr("hidden")
-    newElement.children("td.checkbox-label").text(stuName)
-    val newStu = Student(stuName, newElement)
+    val newElem = $("#stu-tr-template").clone().removeAttr("id").removeAttr("hidden")
+    newElem.children("td.checkbox-label").text(stuName)
+    val newStu = Student(stuName, newElem)
     students :+= newStu
-    newElement.children(".toggle-td").children(".stu-toggle").click(() => newStu.active = !newStu.active)
-    newElement.appendTo("#stu-table")
+    newElem.children(".toggle-td").children(".stu-toggle").click(() => newStu.active = !newStu.active)
+    newElem.appendTo("#stu-table")
   }
 
   private def update(): Unit = {
@@ -44,7 +42,7 @@ object ShareTime {
     if (numActive > 0)
       students.foreach { student =>
         if (student.active) {
-          student.minutesUsed += 1D / 60 / numActive
+          student.addSharedPortionOfThisSecond(numActive)
           val mu = BigDecimal(student.minutesUsed).setScale(1, HALF_UP).toString
           student.progressBar.value(mu)
           student.element.children(".stu-progress-num").text(mu)
@@ -58,4 +56,6 @@ case class Student(name: String, element: JQuery) {
   var active = false
 
   def progressBar: JQuery = element.children(".stu-progress-bar-tr").children("progress")
+
+  def addSharedPortionOfThisSecond(numActive: Int): Unit = minutesUsed += 1D / 60 / numActive
 }
