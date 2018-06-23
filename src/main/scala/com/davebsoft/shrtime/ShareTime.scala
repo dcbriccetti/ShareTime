f368253d6be3ae760749ff47331265ccb5cb6259
Dave Briccetti <daveb@davebsoft.com>
2018-06-22 21:04:23 -0700

    Beautify. Support adding multiple students at once, and setting the number of minutes in the period.

--- src/main/scala/com/davebsoft/shrtime/ShareTime.scala
+++ src/main/scala/com/davebsoft/shrtime/ShareTime.scala
@@ -7,17 +7,31 @@ import scala.math.BigDecimal.RoundingMode.HALF_UP
 
 object ShareTime {
   private var students = Seq[Student]()
+  private var periodMins = 60
 
   def main(args: Array[String]): Unit = $(() => setUpUi())
 
   private def setUpUi() = {
-    $("#add").click(() => addStudent())
+    $("#add").click(() => {
+      val sn = $("#stu-name")
+      addStudents(sn.valueString)
+      sn.value("")
+    })
+    val periodMinsElem = $("#period-mins")
+    periodMinsElem.value(periodMins.toString)
+    periodMinsElem.change(() => {
+      periodMins = periodMinsElem.valueString.toInt
+      students.foreach(_.progressBar.attr("max", periodMins))
+    })
     dom.window.setInterval(() => update(), 1000)
   }
 
-  private def addStudent(): Unit = {
+  private def addStudents(stuNames: String): Unit = {
+    stuNames.split(',').foreach(s => addStudent(s.trim))
+  }
+
+  private def addStudent(stuName: String): Unit = {
     val newElement = $("#stu-tr-template").clone().removeAttr("id").removeAttr("hidden")
-    val stuName = $("#stu-name").valueString
     newElement.children("td.checkbox-label").text(stuName)
     val newStu = Student(stuName, newElement)
     students :+= newStu
@@ -32,7 +46,7 @@ object ShareTime {
         if (student.active) {
           student.minutesUsed += 1D / 60 / numActive
           val mu = BigDecimal(student.minutesUsed).setScale(1, HALF_UP).toString
-          student.element.children(".stu-progress-bar-tr").children("progress").value(mu)
+          student.progressBar.value(mu)
           student.element.children(".stu-progress-num").text(mu)
         }
       }
@@ -42,4 +56,6 @@ object ShareTime {
 case class Student(name: String, element: JQuery) {
   var minutesUsed = 0D
   var active = false
+
+  def progressBar: JQuery = element.children(".stu-progress-bar-tr").children("progress")
 }
